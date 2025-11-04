38fd1b583be49b81dac3240c52f81b7feeffd3e3
gwenael9 <gwenaelgueho@gmail.com>
2025-11-04 15:36:59 +0100

    new archi: création du storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,3 +1,15 @@
 package storage
 
-// structure des données : Contact, type Storer
+type Storer interface {
+	GetAll(c []*Contact) error
+	Add(c *Contact) error
+	Update(id, newName, newMail string) error
+	Delete(c *Contact) error
+	GetUserById(id string) error
+}
+
+type Contact struct {
+	ID    uint
+	Name  string
+	Email string
+}
